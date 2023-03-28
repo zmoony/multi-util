9784a398bccc9938c387e50dd7cdcb7487f096f9
zmoony <zy789789zg@163.com>
2023-03-28 11:28:00 +0800

    netty

--- java-test/src/main/java/com/example/text/demoOnLine/wsdl测试/generated/com/tmri/tfc/webservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.tfc.tmri.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.tmri.tfc.webservice;
